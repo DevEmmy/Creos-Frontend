12ec01ff12997d8d0f4aa19876fcae1616063a85
DevEmmy <eolaosebikan60@gmail.com>
2023-08-13 18:47:25 +0100

    removed icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <title>Creos</title>
         <meta name="description" content="The Future Workforce" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/Logo.png" />
+        {/* <link rel="icon" href="/Logo.png" /> */}
       </Head>
 
       <Homepage />
